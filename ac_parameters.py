4e6ab242ca530a2ec43a9c84cbaef6858d6a5fb6
Schwarzii <J.Yin-5@student.tudelft.nl>
2023-07-20 12:41:52 +0800

    Input Fokker 100 data

--- ac_parameters.py
+++ ac_parameters.py
@@ -6,10 +6,19 @@ fw = 10293  # kg
 plw = 11993  # kg
 mlw = 39915  # kg
 
+
+def trans_top(x):
+    return x * 35.528 / 21.168  # m
+
+
+def trans_side(x):
+    return x * l_f / 20.029  # m
+
+
 # Geometry + loading diagram
 # datum = 2.362  # m, distance to the ac head
 # lemac_arm = 13.335  # m
-lemac_arm = 15.783508692365835
+lemac_arm = trans_top(9.404)
 mac = 3.8326  # m
 
 # From forward to backward
@@ -41,42 +50,34 @@ fuel_center_index = np.array([0, 2, 4, 7, 9, 12, 15, 18, 20, 23, 26, 29, 31, 33]
 fuel_center_max = max(fw - fuel_wing_max, 2512)
 
 # Geometrical and performance parameters
-mlw_n = mlw * 9.81  # N
-rho_0 = 1.225  # kg/m^3
-l_f = 32.501  # m
-l_h = 13.56  # m
-l_fn = 11.05  # m
-l_n = 2.95  # m
-b_ = 27.05  # m
-sweep = 0  # deg
-s = 61  # m^2
-s_net = 32.71  # m^2
-ar = 12  # -
-ar_mod = (1 + 0.3) * ar
-b_h = 7.31  # m
-b_f = 2.7  # m
-s_h = 12  # m^2
-ar_h = 4.4  # -
-h_f = 7.65  # m
-lambda_ = 0.235  # -
-c_r = 4.87  # m
-c_t = 1.16  # m
-c_ = 5.64  # m
-c_f = 1.8  # m
-s_wf = 55.15  # m^2
-e_wing = 0.95  # - (wing efficiency)
-cl_h = -0.8  # - lift coefficient of tail from reference
+mlw_n = mlw * 9.81  # N, maximum landing weight
+rho_0 = 1.225  # kg/m^3, sea-level density
+a_0 = np.sqrt(1.4 * 287.15 * 288.15)  # m/s, sea-level sound speed
+l_f = 32.501  # m, length of fuselage
+l_h = 16  # m, tail arm, distance between 0.25 chord of mac and h tail
+l_fn = trans_top(8.3516985794)  # m, characteristic length of fuselage nose
+l_n = -trans_top(6.0506921084)  # m, characteristic distance of nacelle w.r.t 0.25 MAC
+b = 28.08  # m, wing span
+sweep_le = np.radians(90 - 67.8059412113)  # rad, sweep angle at leading edge of wing
+sweep_quarter = np.radians(17.45)  # rad, sweep angle at 0.25 chord of wing
+sweep_half = np.radians(90 - 76.4340451036)  # rad, sweep angle at 0.5 chord of wing
+sweep_h = np.radians(90 - 66.5184668378)  # rad, sweep angle at 0.5 chord of h tail
+s = 93.5  # m^2, wing area
+ar = 8.43  # -, aspect ratio of wing
+ar_mod = (1 + 0.3) * ar  # -, aspect ratio of wing after modification
+b_h = 10.04  # m, wing span of h tail
+b_f = 3.3  # m, fuselage diameter
+b_n = trans_top(0.9028428563)  # m, width of nacelle
+s_h = 0.232 * s  # m^2, area of h tail
+ar_h = 4.64  # -, aspect ratio of h tail
+h_f = b_f  # m, height of fuselage = fuselage diameter
+taper = 0.235  # -, taper ratio
+c_r = trans_top(3.1782058575)  # m, root chord
+c_t = trans_top(0.8621005777)  # m, tip chord
+s_net = s - c_r * b_f  # m^2, net wing area, wing area - fuselage projected area
 
 # Performance parameters
-r_max_pax = 1370  # km
-v_climb = 87.45  # m/s
-v_max_cruise = 138.89  # m/s
-fuel_cruise = 650  # kg/h
-net_ceiling = 2990  # m
-ho = 7620  # m
-v_cruise = 128.61  # m/s
-R70_pax = 1528  # km
-roc = 6.88  # m/s
-mach = 0.43  # -
-v_app = 61.67  # m/s
+v_cruise = 234.72  # m/s
+mach = 0.77  # -
+v_app = 128.611  # m/s
 v_app_mod = 56.3
