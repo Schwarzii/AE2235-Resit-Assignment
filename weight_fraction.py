07a002684b051e9e7abfb5021638e89ab659d1d2
Schwarzii <J.Yin-5@student.tudelft.nl>
2023-07-20 18:53:40 +0800

    Draw pie plot for Part II

--- weight_fraction.py
+++ weight_fraction.py
@@ -1,8 +1,9 @@
 from pie_setting import *
 from ac_parameters import *
+from CG_calc import Aircraft
 
 
-def pie_plot(oe, fuel, payload):
+def pie_plot(oe, fuel, payload, title=''):
     weights = np.array([oe, fuel, payload])
     weight_type = ["OEW", "Max fuel Weight", "Max payload Weight"]
     labels = [f'{w} kg \n{round(w/mtow * 100, 2)}% MTOW' for w in weights]
@@ -10,8 +11,12 @@ def pie_plot(oe, fuel, payload):
     plt.pie(weights, autopct=percentage, colors=color(weights), labels=labels, startangle=90, wedgeprops=pie_wedges)
     plt.legend(labels=weight_type, title=f'Weight types: \n(MTOW = {mtow} kg)', loc='upper left')
     plt.tight_layout()
-    plt.savefig('plots/part1_weight_pie.png', dpi=300)
+    plt.savefig(f'plots/{title}.png', dpi=300)
     plt.show()
 
 
-pie_plot(oew, fw, plw)
+if __name__ == '__main__':
+    fokker = Aircraft()
+    fokker_mod = Aircraft(mod=True)
+    # pie_plot(fokker.oew, fw, plw, title='part1_weight_pie')
+    pie_plot(fokker_mod.oew, fw, int(plw - fokker_mod.mod[2]), title='part2_weight_pie')
